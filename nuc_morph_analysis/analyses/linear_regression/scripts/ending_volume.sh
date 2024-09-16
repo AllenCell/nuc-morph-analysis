027e8ccf7de6e61e5952d9c5f6ded8224cef0161
Ritvik Vasan <ritvik.vasan@alleninstitute.org>
2024-09-16 08:31:04 -0700

    add scripts for duration,added vol,delta vol prediction

---
+++ nuc_morph_analysis/analyses/linear_regression/scripts/ending_volume.sh
@@ -0,0 +1,5 @@
+python linear_regression_workflow.py \
+--cols 'volume_at_A','volume_at_B','time_at_A','time_at_B','colony_time_at_A','colony_time_at_B','SA_at_B' \
+--alpha_range 0,0.1,0.5,1,1.3,1.5,2,2.5,5,10,11,12,13 \
+--target 'volume_at_C' \
+--save_path "volume_at_C" \
