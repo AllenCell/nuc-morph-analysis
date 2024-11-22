ea6e1adcf2714a5bb8efb809bcdd5a4e642db715
Chris Frick <chris.frick@alleninstitute.org>
2024-11-22 13:54:02 -0800

    restore old global dataset

--- nuc_morph_analysis/lib/preprocessing/global_dataset_filtering.py
+++ nuc_morph_analysis/lib/preprocessing/global_dataset_filtering.py
@@ -21,8 +21,6 @@ from nuc_morph_analysis.analyses.height.add_colony_time import add_colony_time_a
 from nuc_morph_analysis.lib.visualization.plotting_tools import get_plot_labels_for_metric
 from nuc_morph_analysis.lib.preprocessing import labeling_neighbors_helper
 
-
-
 def load_dataset_with_features(
     dataset="all_baseline",
     remove_growth_outliers=True,
@@ -204,9 +202,6 @@ def process_all_tracks(df, dataset, remove_growth_outliers, num_workers):
     df = add_fov_touch_timepoint_for_colonies(df)
     df = add_features.add_non_interphase_size_shape_flag(df)
     df = add_change_over_time(df)
-    df = compute_change_over_time.add_dvdt_over_V(df)
-
-    # df = add_dvdt_over_V(df)
     df = add_neighborhood_avg_features.run_script(df, num_workers=num_workers)
     df = add_neighborhood_avg_features_lrm.run_script(df, num_workers=num_workers, 
                                                 feature_list=["volume", "height", "xy_aspect", "mesh_sa", "2d_area_nuc_cell_ratio"],
@@ -321,6 +316,9 @@ COLUMNS_TO_DROP = [
     "colony_non_circularity",
     "colony_non_circularity_scaled",
     "max_colony_depth",
+    "dxdt_48_volume_per_V",
+    "neighbor_avg_dxdt_48_volume_per_V_90um",
+    "neighbor_avg_dxdt_48_volume_per_V_whole_colony",
     "dataset",
     "height_percentile",
     "raw_full_zstack_path",
@@ -348,7 +346,7 @@ def remove_columns(df, column_list=COLUMNS_TO_DROP):
     return df
 
 
-def add_change_over_time(df, dxdt_feature_list=None, bin_interval_list=None):
+def add_change_over_time(df):
     """
     Adds new columns to the dataframe with the local rate of change for a given feature for a nucleus
 
@@ -356,10 +354,6 @@ def add_change_over_time(df, dxdt_feature_list=None, bin_interval_list=None):
     ----------
     df : pandas.DataFrame
         The input dataframe.
-    dxdt_feature_list : list
-        List of columns to compute growth rates for
-    bin_interval_list : list
-        List of integers, which represents the number of frames to compute growth over
 
     Returns
     -------
@@ -367,7 +361,8 @@ def add_change_over_time(df, dxdt_feature_list=None, bin_interval_list=None):
         The dataframe with the new column added.
     """
     dfm = df.copy()
-    dfm = compute_change_over_time.run_script(dfm, dxdt_feature_list, bin_interval_list,)
+    for bin_interval in compute_change_over_time.BIN_INTERVAL_LIST:
+        dfm = compute_change_over_time.run_script(dfm, bin_interval=bin_interval)
 
     # now check that all columns in df have the same dtype as columns in dfm
     for col in df.columns:
@@ -382,45 +377,6 @@ def add_change_over_time(df, dxdt_feature_list=None, bin_interval_list=None):
         )
     return dfm
 
-def add_volume_change_over_25_minute_window(df, bin_interval=5):
-    """
-    Adds a new column to the dataframe that quantifies how much the volume has changed relative to 
-    25 minutes in the past (units are pixels^3)
-    this is useful for identifying volume dips in all tracks (see Fig S10)
-
-    Parameters
-    ----------
-    df : pandas.DataFrame
-        The input dataframe.
-    bin_interval : int
-        represents the number of frames to compute change in volume over
-        default is 5 frames, which is 25 minutes
-
-    Returns
-    -------
-    df : pandas.DataFrame
-        The dataframe with the new column 'volume_change_over_25_minutes' added.
-        (units are pixels^3)
-    """
-    dfm = df.copy()
-    dfm = compute_change_over_time.run_script(dfm,
-                                               ['volume'],
-                                                 [bin_interval],
-                                                   time_location='end')
-    dfm['volume_change_over_25_minutes'] = dfm['dxdt_5_volume_end']*5
-    
-    # now check that all columns in df have the same dtype as columns in dfm
-    for col in df.columns:
-        if dfm[col].dtype != df[col].dtype:
-            print(f"column {col} has dtype {dfm[col].dtype} in dfm and {df[col].dtype} in df")
-
-    if dfm.shape[0] != df.shape[0]:
-        raise Exception(
-            f"The loaded manifest has {df.shape[0]} rows and your \
-            final manifest has {dfm.shape[0]} rows.\
-            Please revise code to leave manifest rows unchanged."
-        )
-    return dfm
 
 # %%
 if __name__ == "__main__":
