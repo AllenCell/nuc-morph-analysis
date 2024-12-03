594ba2fba1a6e4ade3293889ec9c113eaa2cfb09
Philip Garrison <philip.garrison@alleninstitute.org>
2024-12-03 12:07:15 -0800

    Docstring for type_axlist

--- nuc_morph_analysis/lib/visualization/matplotlib_to_axlist.py
+++ nuc_morph_analysis/lib/visualization/matplotlib_to_axlist.py
@@ -3,8 +3,17 @@ from matplotlib.axes import Axes
 from numpy.typing import NDArray
 
 def type_axlist(plt_axis_np: Union[Axes, NDArray]) -> List[Axes]:
-        # Figure.subplots returns Axes or array of Axes
-        # https://github.com/numpy/numpy/issues/24738
+    """
+    Figure.subplots returns Axes or array of Axes, but it's improperly typed
+    See discussion here:
+    https://github.com/numpy/numpy/issues/24738
+
+    Example:
+    >>> fig,axlist_untyped = plt.subplots(nrows=2,ncols=1,figsize=(6.5,8))
+    >>> axlist: List[Axes] = type_axlist(axlist_untyped)
+    """
+    # This function was adapted from this PR, licensed under BSD-3:
+    # https://github.com/pytorch/captum/pull/1416/files
     plt_axis_list: List[Axes] = []
     if type(plt_axis_np) == Axes:
         plt_axis_list = [plt_axis_np]
