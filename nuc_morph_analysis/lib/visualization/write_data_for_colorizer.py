d2cbd5a347fdffcb47c51ee37f3415def7ee30e4
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2024-12-06 16:40:37 -0800

    remove power law fit

--- nuc_morph_analysis/lib/visualization/write_data_for_colorizer.py
+++ nuc_morph_analysis/lib/visualization/write_data_for_colorizer.py
@@ -244,7 +244,6 @@ FEATURE_COLUMNS = {
             categories=["False", "True"],
         ), 
         NucMorphFeatureSpec('volume_change_over_25_minutes'),
-        NucMorphFeatureSpec('power_fit_volume'),
     ],
 }
 
@@ -419,6 +418,7 @@ def make_dataset(
 
     # load the dataset once
     df_all = load_dataset_with_features("all_baseline", remove_growth_outliers=False)
+    df_all['volume_dips_peak_mask_at_region'] = df_all['volume_dips_peak_mask_at_region'].astype(str)
 
     for filter in filters:
         output_dir_subset = Path(output_dir) / filter
