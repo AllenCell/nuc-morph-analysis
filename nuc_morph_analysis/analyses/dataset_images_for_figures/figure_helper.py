b6dc94b1a4b10493205b40ed22e22a5718f6ef6c
Julie Dixon <juliec@alleninstitute.org>
2024-12-07 13:03:30 -0800

    fix frame num for seg check

--- nuc_morph_analysis/analyses/dataset_images_for_figures/figure_helper.py
+++ nuc_morph_analysis/analyses/dataset_images_for_figures/figure_helper.py
@@ -993,7 +993,7 @@ def process_images_and_add_to_dataframe(df_fmb, df, seg_img_list, raw_img_list):
             df_fmb.loc[timepoint, f"seg_{yx_zx_zy}"] = [seg_slices[i]]
 
             # now retrieve the contours
-            if label_img_val > 0 and (timepoint + 2) < (predicted_breakdown + 1):
+            if label_img_val > 0 and timepoint < (predicted_breakdown + 1):
                 # only draw the contour if the cell is present in the segmentation image
                 # and the timepoint is before breakdown
                 # identify the contours from the label image and save them as matplotlib Polygons
